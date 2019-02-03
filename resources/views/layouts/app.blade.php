5cc9befc5dff0fe23d455aa2cc43b062e23f419f
tsubasa.miyamoto <tobymiyamoto@gmail.com>
2019-02-03 10:59:16 +0000

    CRUD messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <body>
         <header class="mb-4">
             <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">TaskList</a>
+                <a class="navbar-brand" href="/">TaskBoard</a>
                 
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
                     <span class="navbar-toggler-icon"></span>
@@ -19,10 +19,13 @@
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
+                         <li class="nav-item">{!! link_to_route('tasks.create', '新規タスク投稿', [], ['class' => 'nav-link']) !!}</li>
                     </ul>
                 </div>
             </nav>
         </header>
+        
+      
         
         <div class="container">
             @yield('content')
