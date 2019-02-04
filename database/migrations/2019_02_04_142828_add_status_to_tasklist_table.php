1b7548387fe90bdef5539fc219ecec276063afb3
tsubasa.miyamoto <tobymiyamoto@gmail.com>
2019-02-04 08:46:54 +0000

    correction migration file

--- database/migrations/2019_02_04_142828_add_status_to_tasklist_table.php
+++ database/migrations/2019_02_04_142828_add_status_to_tasklist_table.php
@@ -14,7 +14,7 @@ class AddStatusToTasklistTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+           $table->char('status', 10);
         });
     }
 
@@ -26,7 +26,7 @@ class AddStatusToTasklistTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 }
