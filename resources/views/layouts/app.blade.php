910f8dc42648e3492fd4f4b4c85f81d603538018
tsubasa.miyamoto <tobymiyamoto@gmail.com>
2019-02-09 19:13:13 +0000

    top page added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,38 +2,20 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>TaskList</title>
+        <title>Tasklist</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     </head>
 
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">TaskBoard</a>
-                
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                         <li class="nav-item">{!! link_to_route('tasks.create', '新規タスク投稿', [], ['class' => 'nav-link']) !!}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        
-      
+
+        @include('commons.navbar')
         
         <div class="container">
             @include('commons.error_tasks')
             
             @yield('content')
         </div>
-        
-        
         
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', 'TasksController@index');
-Route::resource('tasks', 'TasksController');
+/*Route::get('/', 'TasksController@index');
+Route::resource('tasks', 'TasksController');*/
 
